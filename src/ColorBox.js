7906532273c4c5725713b7de97704adcc06291d7
Devin Arnold <dbarnold321@gmail.com>
2019-08-05 16:51:55 -0600

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -4,12 +4,13 @@ import { throws } from 'assert';
 export default class ColorBox extends Component {
   
   render() {
-    return (
-      <div className="color-box" style={{opacity: this.props.opacity}}>
-        {this.props.opacity >= 0.2 ? <ColorBox opacity={this.props.opacity - 0.1} /> : null}
-      </div>
-    )
-  }
+      if (this.props.opacity >= 0.2){  
+        return(
+        <div className="color-box" style={{opacity: this.props.opacity}}>
+          <ColorBox opacity={this.props.opacity - 0.1} />
+        </div>)
+      }else return (null)}
+      
   
 }
 
