2c3ef93690a667daec2ff71cbd55c93b0d9cb465
Blake A. Long <blake.a.long@gmail.com>
2020-04-17 17:35:01 -0600

    Done.

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -17,8 +17,7 @@ export default class BlogPost extends Component {
         Just like hopes springing high,<br/>
         Still I'll rise.<br/>
         -Maya Angelou<br/>
-
-        (remove the comment ticks and their surrounding brackets)
+        
         <Comment commentText={comments[0]} />
         <Comment commentText={comments[1]} />
         <Comment commentText={comments[2]} />
